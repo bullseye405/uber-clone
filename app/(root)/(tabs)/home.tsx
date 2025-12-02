056f8c3941e955e2947c77b8f949bcb2c30e966b
Samir Prajapati <bullseye405@gmail.com>
2025-12-02 15:11:47 +0545

    completed auth screens

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,29 @@
+// import { SignOutButton } from "@/app/components/SignOutButton";
+import { SignOutButton } from "@/components/SignOutButton";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Home = () => {
+export default function Page() {
+  const { user } = useUser();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <SignedIn>
+          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+          <SignOutButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href="/(auth)/sign-in">
+            <Text>Sign in</Text>
+          </Link>
+          <Link href="/(auth)/sign-up">
+            <Text>Sign up</Text>
+          </Link>
+        </SignedOut>
+      </View>
+    </SafeAreaView>
   );
-};
-
-export default Home;
+}
