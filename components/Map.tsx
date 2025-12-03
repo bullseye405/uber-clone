3593b0aec6d1c7ea78b4f0f5e84d35e46246f007
Samir Prajapati <bullseye405@gmail.com>
2025-12-03 15:02:42 +0545

    add map packages

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,23 @@
 import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+  // const region = {};
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <View>
+        <Text>Map</Text>
+      </View>
+    </MapView>
   );
 };
 
